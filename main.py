021898330a79fbb4bd0f2d6cab3fe15b3b413052
Yun Jae <yunjae@Yuns-Air.lan>
2023-08-24 18:50:22 -0600

    added a kelvin to fahrenheit converter function, and printed an example of the get request working

--- main.py
+++ main.py
@@ -5,8 +5,15 @@ BASE_URL = "http://api.openweathermap.org/data/2.5/weather?"
 API_KEY = "a4243aa3498ba0bab7d3f32f82b18ef8"
 CITY = "Denver"
 
-url = BASE_URL + "appid=" + API_KEY + "&q=" + CITY
+def k_ftemp(kelvin):
+    celsius = kelvin - 273.15
+    fahrenheit = celsius * (9/5) + 32
+    return fahrenheit
 
+
+url = BASE_URL + "appid=" + API_KEY + "&q=" + CITY
 response = requests.get(url).json()
 
-print(response)
+fahrenheit_temp = round(k_ftemp(response["main"]["temp"]))
+
+print(f"{CITY} is currently {fahrenheit_temp} degrees fahrenheit!")
