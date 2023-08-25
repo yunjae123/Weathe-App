abaa27808f6528569e519f8dadc706242682b341
Yun Jae <yunjae@Yuns-Air.lan>
2023-08-24 18:21:28 -0600

    fixed data/3.0 to data/2.5 because it was returning a 404 error

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import datetime as dt
 import requests 
 
-BASE_URL = "http://api.openweathermap.org/data/3.0/weather?"
+BASE_URL = "http://api.openweathermap.org/data/2.5/weather?"
 API_KEY = "a4243aa3498ba0bab7d3f32f82b18ef8"
 CITY = "Denver"
 
 url = BASE_URL + "appid=" + API_KEY + "&q=" + CITY
 
-response = requests.get(url)
+response = requests.get(url).json()
+
+print(response)
