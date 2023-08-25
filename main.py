de3db5d564ab5d8ac981a5b19e8b0542f46bee17
Yun Jae <yunjae@Yuns-Air.lan>
2023-08-25 14:12:11 -0600

    Add index function, and return render_template with all the variables from another file I'm gonna create soon

--- main.py
+++ main.py
@@ -1,21 +1,14 @@
 import datetime as dt
 import requests 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
-
-@app.route('/')
-
-def index():
-    return "Hello World!!!"
-
-if __name__ == "__main__":
-    app.run(debug=True)
-
 
 BASE_URL = "http://api.openweathermap.org/data/2.5/weather?"
 API_KEY = "a4243aa3498ba0bab7d3f32f82b18ef8"
 CITY = "Seoul"
+
+@app.route('/', methods=["GET", "POST"])
 
 def k_ftemp(kelvin):
     celsius = kelvin - 273.15
@@ -26,14 +19,26 @@ def k_ctemp(kelvin):
     return kelvin - 273.1
 
 
-url = BASE_URL + "appid=" + API_KEY + "&q=" + CITY
-response = requests.get(url).json()
+def index():
+    city = CITY
+    if request.method == "POST":
+        city = request.form.get("city")
+    url = BASE_URL + "appid=" + API_KEY + "&q=" + CITY
+    response = requests.get(url).json()
+    fahrenheit_temp = round(k_ftemp(response["main"]["temp"]))
+    celsius_temp = round(k_ctemp(response["main"]["temp"]))
+    humidity = response['main']['humidity']
+    local_sunrise_time = dt.datetime.utcfromtimestamp(response['sys']['sunrise'] + response['timezone'])
+    local_sunset_time = dt.datetime.utcfromtimestamp(response['sys']['sunset'] + response['timezone'])
+    return render_template("index.html", city = city, fahrenheit_temp = fahrenheit_temp, celsius_temp = celsius_temp, humidity = humidity)
 
-fahrenheit_temp = round(k_ftemp(response["main"]["temp"]))
-celsius_temp = round(k_ctemp(response["main"]["temp"]))
-humidity = response['main']['humidity']
-local_sunrise_time = dt.datetime.utcfromtimestamp(response['sys']['sunrise'] + response['timezone'])
-local_sunset_time = dt.datetime.utcfromtimestamp(response['sys']['sunset'] + response['timezone'])
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
 
 # print(f"{CITY} is currently {fahrenheit_temp}°F or {celsius_temp}°C.")
 # print(f"The humidity in {CITY} is currently {humidity}%.")
