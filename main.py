5d8954520790d24fd4d2a9613f3b9961cd3f8d04
Yun Jae <yunjae@Yuns-Air.lan>
2023-08-25 15:36:54 -0600

    Make the website handle non-existant cities, error codes other than 404 and also have the default city set

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ app = Flask(__name__)
 
 BASE_URL = "http://api.openweathermap.org/data/2.5/weather?"
 API_KEY = "a4243aa3498ba0bab7d3f32f82b18ef8"
+DEFAULT_CITY = "Denver"
+
 
 def k_ftemp(kelvin):
     celsius = kelvin - 273.15
@@ -18,19 +20,29 @@ def k_ctemp(kelvin):
 @app.route('/', methods=["GET", "POST"])
 
 def index():
+    city = DEFAULT_CITY
     if request.method == "POST":
-        city = request.form.get("city")
+        city = request.form.get("city") or city
     url = BASE_URL + "appid=" + API_KEY + "&q=" + city
-    response = requests.get(url).json()
-    fahrenheit_temp = round(k_ftemp(response["main"]["temp"]))
-    celsius_temp = round(k_ctemp(response["main"]["temp"]))
-    humidity = response['main']['humidity']
-    local_sunrise_time = dt.datetime.utcfromtimestamp(response['sys']['sunrise'] + response['timezone'])
-    local_sunset_time = dt.datetime.utcfromtimestamp(response['sys']['sunset'] + response['timezone'])
-    return render_template("index.html", city = city, fahrenheit_temp = fahrenheit_temp, celsius_temp = celsius_temp, humidity = humidity)
+    response = requests.get(url)
 
+    if response.status_code == 200:
+        data = response.json()
+        fahrenheit_temp = round(k_ftemp(data["main"]["temp"]))
+        celsius_temp = round(k_ctemp(data["main"]["temp"]))
+        humidity = data['main']['humidity']
+        local_sunrise_time = dt.datetime.utcfromtimestamp(data['sys']['sunrise'] + data['timezone'])
+        local_sunset_time = dt.datetime.utcfromtimestamp(data['sys']['sunset'] + data['timezone'])
+        return render_template("index.html", city = city, fahrenheit_temp = fahrenheit_temp, celsius_temp = celsius_temp, humidity = humidity)
+    
+    elif response.status_code == 404:
+        return render_template("index.html", city=city, error="City not found")
+    
+    else:
+        return render_template("index.html", city=city, error="An unexpected error occurred.")
 if __name__ == "__main__":
     app.run(debug=True)
+
 
 
 
